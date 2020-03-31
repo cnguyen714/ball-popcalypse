8caf220b5857449ae9a7d275571807392a9c2dbc
Calvin Nguyen <calvindnguyen714@gmail.com>
2020-03-31 02:25:31 -0700

    Expand Trig library functions

--- javascript/lib/Trig.js
+++ javascript/lib/Trig.js
@@ -1,8 +1,22 @@
 import Vector from "../lib/Vector";
 
 class Trig {
+  
+  // where 0 is the positive x-axis, going clockwise;
+  static getVectorfromDegrees(degrees) {
+    let angle = degrees / 180 * Math.PI;
+    
+   return new Vector(Math.sin(angle), Math.cos(angle));
+  }
 
-  static getVectorfromDegrees(degree)
+  static rotateByDegree(vector, degrees) {
+    let angle = Math.atan2(vector.y, vector.x);
+    let length = vector.length();
+
+    angle += degrees / 180 * Math.PI;
+
+    return (new Vector(Math.sin(angle), Math.cos(angle)).multiply(length));
+  }
 }
 
 export default Trig;
